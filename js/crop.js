4f3e3629569bae6259c54d543e8bbc1ce15420d6
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-02-27 17:54:03 -0800

    Added documentation again

--- js/crop.js
+++ js/crop.js
@@ -1,7 +1,11 @@
 ﻿/**
  * CropJS ---------------------- DOCUMENTATION NOT COMPLETE
  * This is a JavaScript widget that adds end-user cropping functionality to any image
- * on a webpage with a number of configurable options. It makes use of the KineticJS library. 
+ * on a webpage with a number of configurable options. It makes use of the KineticJS
+ * library. The interface works with mouse as well as touch.
+ * The interface lets people select any region whcih can then be dragged and resized
+ * with customizable handles. The selected region can be dropped simply by selecting
+ * a new region with the mouse. 
  */
 
 
